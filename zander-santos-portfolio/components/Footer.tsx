72e190ac75301ca103fd69d4ac385a6c642735a1
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-05 11:40:25 -0500

    Created a Footer component with contact links

---
+++ zander-santos-portfolio/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>Designed and Built by Zander Santos</p>
+      <div>
+        <a href="mailto:zander.santos.dev@gmail.com">Email</a>
+        <a href="https://github.com/zandersantos">GitHub</a>
+        <a href="https://www.linkedin.com/in/zander-santos/">LinkedIn</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
