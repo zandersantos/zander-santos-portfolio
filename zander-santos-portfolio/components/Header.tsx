3f76a02c25204c619553c1e6be178b46634d2c4d
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-05 11:22:02 -0500

    Started a basic header and updated the root layout with header and metadata

---
+++ zander-santos-portfolio/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <button>About</button>
+      <button>Experience</button>
+      <button>Education</button>
+      <button>Projects</button>
+      <button>Contact</button>
+    </div>
+  )
+}
+
+export default Header;
