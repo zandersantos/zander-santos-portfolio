f4b9d6b6b70572e6b52f83b10397f1232dcdc915
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-06 00:01:48 -0500

    Fixed the Tailwind config errors

--- zander-santos-portfolio/tailwind.config.ts
+++ zander-santos-portfolio/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -8,5 +9,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
 
+export default config;
