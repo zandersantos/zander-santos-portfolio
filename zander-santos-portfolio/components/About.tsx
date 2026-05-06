9216e6fc0ec7367890ce2f57180c01f83427b883
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-06 18:56:18 -0500

    Updated the About styling, added and info section, and added About to the page

--- zander-santos-portfolio/components/About.tsx
+++ zander-santos-portfolio/components/About.tsx
@@ -1,6 +1,20 @@
 const About = () => {
-  return (
-    <h2>About</h2>
+    return (
+    <section id="about" className="min-h-screen flex items-center px-8">
+      <div className="max-w-3xl mx-auto">
+        <h2 className="text-md uppercase text-neutral-400 mb-4">About</h2>
+
+        <p className="text-xl leading-relaxed text-neutral-600 font-light mb-12">
+          I&apos;m a full-stack developer based in Winnipeg who&apos;s passionate about building things
+          that matter. I thrive on collaboration and communication, translating ideas into
+          solutions that work for different audiences. I&apos;m constantly exploring emerging
+          technologies and asking &quot;what&apos;s possible?&quot; More than anything, I love learning.
+          Complex problems energize me, and I&apos;m always looking for opportunities to expand
+          my knowledge and grow.
+        </p>
+      </div>
+    </section>
+
   );
 };
 
