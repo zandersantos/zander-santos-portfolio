e63ecf4344aae12259b1ebb564f8ff65f186b82c
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-06 18:52:02 -0500

    Updated styling to the name and title

--- zander-santos-portfolio/components/Hero.tsx
+++ zander-santos-portfolio/components/Hero.tsx
@@ -1,10 +1,15 @@
 const Hero = () => {
   return (
-    <div>
-      <h1>Zander Santos</h1>
-      <h2>Software & Fullstack Developer</h2>
+    <div className="min-h-screen flex items-center justify-center px-8 py-8 space-x-8">
+      <h1 className="text-6xl font-bold">
+        Zander Santos
+      </h1>
+      <p className="text-6xl font-semibold text-gray-400">|</p>
+      <h2 className="text-6xl font-semibold text-gray-400">
+        Fullstack Developer
+      </h2>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
