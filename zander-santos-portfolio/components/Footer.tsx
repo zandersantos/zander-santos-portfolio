999b56ad1d7d7700f9a93640a0b0c30bdbccfb4b
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-06 00:38:02 -0500

    Updated Footer styling and added a footer element

--- zander-santos-portfolio/components/Footer.tsx
+++ zander-santos-portfolio/components/Footer.tsx
@@ -1,13 +1,14 @@
 const Footer = () => {
   return (
-    <div>
-      <p>Designed and Built by Zander Santos</p>
-      <div>
-        <a href="mailto:zander.santos.dev@gmail.com">Email</a>
-        <a href="https://github.com/zandersantos">GitHub</a>
-        <a href="https://www.linkedin.com/in/zander-santos/">LinkedIn</a>
+    <footer className="flex flex-col items-center justify-center py-8 text-center">
+      <div className="mb-2 flex gap-4">
+        <a href="mailto:zander.santos.dev@gmail.com" className="footer-link">Email</a>
+        <a href="https://github.com/zandersantos" className="footer-link">GitHub</a>
+        <a href="https://www.linkedin.com/in/zander-santos/" className="footer-link">LinkedIn</a>
       </div>
-    </div>
+
+      <p>Designed and Built by Zander Santos - 2026</p>
+    </footer>
   )
 }
 
