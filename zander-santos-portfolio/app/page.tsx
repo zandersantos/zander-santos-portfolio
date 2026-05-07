c2ec312f96170322870a4e9bcbf2d33582af30e8
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-07 12:02:34 -0500

    Created a new Experience component and displayed it on the page

--- zander-santos-portfolio/app/page.tsx
+++ zander-santos-portfolio/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Hero/>
       <About/>
+      <Experience/>
     </div>
 
   );
---
+++ zander-santos-portfolio/components/Experience.tsx
@@ -0,0 +1,8 @@
+const Experience = () => {
+
+  return (
+    <h2>Experience</h2>
+  );
+};
+
+export default Experience;
