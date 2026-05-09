e763384c3867c927fcb0a962e7546479d00b7a8b
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2026-05-09 17:55:56 -0500

    Added security attributes to the footers external links

--- zander-santos-portfolio/components/Footer.tsx
+++ zander-santos-portfolio/components/Footer.tsx
@@ -2,9 +2,28 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center py-8 text-center">
       <div className="mb-2 flex gap-4">
-        <a href="mailto:zander.santos.dev@gmail.com" className="footer-link">Email</a>
-        <a href="https://github.com/zandersantos" className="footer-link">GitHub</a>
-        <a href="https://www.linkedin.com/in/zander-santos/" className="footer-link">LinkedIn</a>
+        <a
+          href="mailto:zander.santos.dev@gmail.com"
+          className="footer-link"
+        >
+          Email
+        </a>
+        <a
+          href="https://github.com/zandersantos"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/zander-santos/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          LinkedIn
+        </a>
       </div>
 
       <p>Designed and Built by Zander Santos - 2026</p>
